15fbaf800063c37d866efbfb3d23dda2627aead1
Vidush Singhal <vidush.sl@gmail.com>
2025-05-30 15:40:04 -0700

    edits

--- swap.c
+++ swap.c
@@ -1,13 +1,10 @@
 //@requires ptr r1
 //@requires ptr r2
-//@ensures ptr r1
-//@ensures ptr r2
+//@ensures  ptr r1
+//@ensures  ptr r2
 void ref_swap(int* r1, int* r2) 
 {
-  //bb
   int tmp = *r1;
-  //cc
   *r1 = *r2;
-  //aaaaa
   *r2 = tmp;
 }
