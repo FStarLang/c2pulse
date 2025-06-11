ebd69371aed4699031873dcb8ca562b4b0971a4c
Angelica Moreira <48168649+angelica-moreira@users.noreply.github.com>
2025-06-11 10:08:43 -0700

    Updating the location extractor

--- include/ExprLocationAnalyzer.h
+++ include/ExprLocationAnalyzer.h
@@ -20,38 +20,22 @@ public:
 
   void analyze(clang::TranslationUnitDecl *TU);
 
+  // Needed for extracting function metadata
   bool VisitFunctionDecl(clang::FunctionDecl *FD);
-  bool VisitVarDecl(clang::VarDecl *VD);
+
+  // Needed for variable declarations like `int tmp = *r1;`
+  bool VisitDeclStmt(clang::DeclStmt *DS);
+
+  // Needed for assignments like `*r1 = *r2;`
   bool VisitBinaryOperator(clang::BinaryOperator *BO);
-  bool VisitUnaryOperator(clang::UnaryOperator *UO);
-  bool VisitCallExpr(clang::CallExpr *CE);
-  bool VisitIntegerLiteral(clang::IntegerLiteral *IL);
-  bool VisitFloatingLiteral(clang::FloatingLiteral *FL);
-  bool VisitCharacterLiteral(clang::CharacterLiteral *CL);
-  bool VisitStringLiteral(clang::StringLiteral *SL);
-  bool VisitParenExpr(clang::ParenExpr *PE);
-  bool VisitImplicitCastExpr(clang::ImplicitCastExpr *ICE);
-  bool VisitCStyleCastExpr(clang::CStyleCastExpr *CCE);
-  bool VisitDeclRefExpr(clang::DeclRefExpr *DRE);
-  bool VisitMemberExpr(clang::MemberExpr *ME);
-  bool VisitConditionalOperator(clang::ConditionalOperator *CO);
-  bool VisitArraySubscriptExpr(clang::ArraySubscriptExpr *ASE);
-  bool VisitCompoundLiteralExpr(clang::CompoundLiteralExpr *CLE);
-  bool VisitImplicitValueInitExpr(clang::ImplicitValueInitExpr *IVIE);
-  bool VisitInitListExpr(clang::InitListExpr *ILE);
-  bool VisitLambdaExpr(clang::LambdaExpr *LE);
-  bool VisitUnaryExprOrTypeTraitExpr(clang::UnaryExprOrTypeTraitExpr *SOAE);
-  bool VisitCXXConstructExpr(clang::CXXConstructExpr *CCE);
-  bool VisitCXXNewExpr(clang::CXXNewExpr *NewE);
-  bool VisitCXXDeleteExpr(clang::CXXDeleteExpr *DelE);
 
 private:
   clang::ASTContext &Context;
   const clang::SourceManager &SM;
-  std::string CurrentFunctionName = ""; 
+  std::string CurrentFunctionName = "";
 
   bool shouldProcess() const;
+
   void printExprInfo(const std::string &label, const clang::Expr *E);
   void printSourceLine(clang::SourceLocation loc);
-  void prettyPrint(const clang::Expr *E);
 };
