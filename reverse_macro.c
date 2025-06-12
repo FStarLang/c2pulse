84b623b2a27f7abe99bffd9d13e4b6101dfac98c
Vidush Singhal <vidush.sl@gmail.com>
2025-06-12 09:33:58 -0700

    deduce array types

--- reverse_macro.c
+++ reverse_macro.c
@@ -7,11 +7,11 @@
 #if __PULSE_VERIFICATION
  void __pulseproofterm(const char *lemma);
  void __pulsewhileinvariant(const char* invariant, const char* lemma);
- #define PROOFTERM(l) __pulseproofterm(#l)
+ #define PROOFTERM(l) __pulseproofterm(#l);
  #define WHILEINVARIANT(i, l) __pulsewhileinvariant(#i, #l);
- #define META_WHILE(condition, meta) \
-    meta \
-    while (condition) 
+ #define META_WHILE(condition, ...) \
+     __VA_ARGS__ \
+     while (condition)    
 #else
  #define PROOFTERM(s)
  #define WHILEINVARIANT(s, v)
@@ -24,27 +24,35 @@
 ///@ensures  exists* s. arr |-> s
 void reverse(uint32_t *arr, size_t len)
 {
-size_t i = 0;
-META_WHILE(i < len / 2, WHILEINVARIANT(invariant b., 
+  size_t i = 0;
+  META_WHILE(i < len / 2, 
+   WHILEINVARIANT(invariant b., 
                     exists* vi.
                     (i |-> vi) **
                     (exists* s. arr |-> s) **
                     pure (b == (vi `SizeT.lt` SizeT.div len 2sz))
-                  )) {
-size_t __anf_tmp1 = len - 1;
-size_t __anf_tmp2 = __anf_tmp1 - i;
-size_t j = __anf_tmp2;
-PROOFTERM(pts_to_len arr);
-uint32_t tmp = arr[i];
-uint32_t __anf_tmp4 = arr[j];
-arr[i] = __anf_tmp4;
-arr[j] = tmp;
-i = i + 1;
+                    )
+                    ) 
+  {
+    size_t __anf_tmp1 = len - 1;
+    size_t __anf_tmp2 = __anf_tmp1 - i;
+    size_t j = __anf_tmp2;
+    PROOFTERM(pts_to_len arr)
+    uint32_t tmp = arr[i];
+    uint32_t __anf_tmp4 = arr[j];
+    arr[i] = __anf_tmp4;
+    arr[j] = tmp;
+    i = i + 1;
+  }
 }
 
-}
+// int main () {
+//   return 0;
+// }
 
 
-int main () {
-return 0;
-}
+
+  //  WHILEINVARIANT(invariant c, exists* vi.
+  //                   (i |-> vi) **
+  //                   (exists* s. arr |-> s) **
+  //                   pure (c == (vi `SizeT.lt` SizeT.div len 2sz)))
