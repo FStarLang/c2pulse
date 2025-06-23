856eb8f495beeb327db7939f0d57dc75d6b02891
Angelica Moreira <48168649+angelica-moreira@users.noreply.github.com>
2025-06-23 14:50:44 -0700

    Updating output

--- test/swap_test.c
+++ test/swap_test.c
@@ -12,20 +12,25 @@ void ref_swap(int* r1, int* r2)
   *r2 = tmp;
 }
 
+// CHECK: Print the Pulse function Definition:
+
 // CHECK: The pulse function Name is: ref_swap
 
-// CHECK: Print Program:
+// CHECK: Print function arguments: (r1,ref Int32.t),(r2,ref Int32.t),
 
-// CHECK: fn ref_swap
-// CHECK-NEXT: (r1 : ref Int32.t)
-// CHECK-NEXT: (r2 : ref Int32.t)
-// CHECK: {
-// CHECK-NEXT: let tmp = (! r1);
-// CHECK-NEXT: r1 := (! r2);
-// CHECK-NEXT: r2 := tmp;
-// CHECK: }
+// CHECK: Print annotations: 
 
-// CHECK: Success: Code transformed and syntax validated.
+// CHECK: Requires (r1 `pts_to` 'w1) ** (r2 `pts_to` 'w2)
+// CHECK: Ensures (r1 `pts_to` 'w2) ** (r2 `pts_to` 'w1)
+
+
+// CHECK: Print the function body: 
+
+// CHECK: let tmp = (! r1)
+// CHECK: r1 := (! r2)
+// CHECK: r2 := tmp
+
+// CHECK: End printing the function Definition
 
 // PULSE: Verified module: Swap
 // PULSE-NEXT: All verification conditions discharged successfully
