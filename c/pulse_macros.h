3b4dac3567d69275f79ba182844b55de0d529853
Vidush Singhal <vidush.sl@gmail.com>
2025-06-24 23:30:13 -0700

    fix bugs

--- c/pulse_macros.h
+++ c/pulse_macros.h
@@ -1,4 +1,4 @@
-#define STR(s) #s
+#define STR(...) #__VA_ARGS__
 #define REQUIRES(s) [[clang::pulse("requires:" STR(s)"|END")]]
 #define ENSURES(s) [[clang::pulse("ensures:" STR(s)"|END")]]
 #define RETURNS(s) [[clang::pulse("returns:" STR(s)"|END")]]
@@ -6,4 +6,6 @@
 #define INVARIANTS(...) [[clang::pulse("invariants:" STR(__VA_ARGS__)"|END")]]
 #define LEMMA(l) [[clang::pulse("lemma:" STR(l)"|END")]]
 #define ERASED_ARG(arg) [[clang::pulse("erased_arg:" STR(arg)"|END")]]
-#define ASSERT(message) [[clang::pulse("assert:" STR(message)"|END")]]
+#define ASSERT(message) [[clang::pulse("assert:" STR(message) "|END")]]
+#define HEAPALLOCATED(message)                                                 \
+  [[clang::pulse("heap_allocated:" STR(message) "|END")]]
