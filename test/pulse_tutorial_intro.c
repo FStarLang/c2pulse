19b3106b6604a518af31344b0db4fa2cf568fa2f
Nikhil Swamy <nikswamy@users.noreply.github.com>
2025-07-01 13:37:49 -0700

    adding an extra default open until issue #3 is fixed

--- test/pulse_tutorial_intro.c
+++ test/pulse_tutorial_intro.c
@@ -3,8 +3,8 @@
 #include "../include/PulseMacros.h"
 
 REQUIRES("x |-> 'i")
-REQUIRES("pure FStar.Int32.(fits (v 'i + 1))")
-ENSURES("exists* j. (x |-> j) ** pure FStar.Int32.(v 'i + 1 == v j)")
+REQUIRES("pure (fits (as_int 'i + 1))")
+ENSURES("exists* j. (x |-> j) ** pure (as_int 'i + 1 == as_int j)")
 void incr (int *x)
 {
     *x = *x + 1;
@@ -12,8 +12,8 @@ void incr (int *x)
 
 ERASED_ARG(#i:_)
 REQUIRES(x |-> i)
-REQUIRES(pure FStar.Int32.(fits (v i + 1)))
-ENSURES("exists* j. (x |-> j) ** pure FStar.Int32.(v i + 1 == v j)")
+REQUIRES(pure (fits (v i + 1)))
+ENSURES("exists* j. (x |-> j) ** pure (as_int i + 1 == as_int j)")
 void incr_explicit_i (int *x)
 {
     *x = *x + 1;
@@ -23,8 +23,8 @@ ERASED_ARG(#i:_)
 ERASED_ARG(#j:_)
 REQUIRES(x |-> i)
 REQUIRES(y |-> j)
-REQUIRES(pure FStar.Int32.(fits (v i + 1)))
-ENSURES("exists* j. (x |-> j) ** pure FStar.Int32.(v i + 1 == v j)")
+REQUIRES(pure (fits (v i + 1)))
+ENSURES("exists* j. (x |-> j) ** pure (v i + 1 == v j)")
 ENSURES(y |-> j)
 void incr_frame(int *x, int *y)
 {
