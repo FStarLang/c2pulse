566e054f50991cbeabe4b947ec32400f83bd578e
Nikhil Swamy <nikswamy@users.noreply.github.com>
2025-06-30 22:26:28 -0700

    adding some tests, very early beginnings of Pulse.Lib.C support library, etc.

--- test/pulse_tutorial_intro.c
+++ test/pulse_tutorial_intro.c
@@ -1,29 +1,31 @@
 #include <stdint.h>
 #include <stdlib.h>
-#include "../pulse_macros.h"
+#include "../include/PulseMacros.h"
 
 REQUIRES("x |-> 'i")
 REQUIRES("pure FStar.Int32.(fits (v 'i + 1))")
-ENSURES("x |-> ('i + 1)")
+ENSURES("exists* j. (x |-> j) ** pure FStar.Int32.(v 'i + 1 == v j)")
 void incr (int *x)
 {
     *x = *x + 1;
 }
 
-ERASED_ARG(i)
+ERASED_ARG(#i:_)
 REQUIRES(x |-> i)
 REQUIRES(pure FStar.Int32.(fits (v i + 1)))
-ENSURES(x |-> (i + 1))
+ENSURES("exists* j. (x |-> j) ** pure FStar.Int32.(v i + 1 == v j)")
 void incr_explicit_i (int *x)
 {
     *x = *x + 1;
 }
 
-ERASED_ARG(i)
-ERASED_ARG(j)
-REQUIRES(x |-> i ** y |-> j)
+ERASED_ARG(#i:_)
+ERASED_ARG(#j:_)
+REQUIRES(x |-> i)
+REQUIRES(y |-> j)
 REQUIRES(pure FStar.Int32.(fits (v i + 1)))
-ENSURES(x |-> (i + 1) ** y |-> j)
+ENSURES("exists* j. (x |-> j) ** pure FStar.Int32.(v i + 1 == v j)")
+ENSURES(y |-> j)
 void incr_frame(int *x, int *y)
 {
     incr(x);
