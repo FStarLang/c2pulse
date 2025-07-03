34cf564979cc57d597072fbe842ac52b92285587
Vidush Singhal <vidush.sl@gmail.com>
2025-07-03 14:21:41 -0700

    Make macro expansion conditional: Fixes #20

--- include/PulseMacros.h
+++ include/PulseMacros.h
@@ -1,5 +1,7 @@
+#define C2PULSE
+#ifdef C2PULSE
 #define STR(...) #__VA_ARGS__
-#define REQUIRES(s) [[clang::pulse("requires:"#s"|END")]] 
+#define REQUIRES(s) [[clang::pulse("requires:" STR(s)"|END")]] 
 #define ENSURES(s) [[clang::pulse("ensures:" STR(s)"|END")]]
 #define RETURNS(s) [[clang::pulse("returns:" STR(s)"|END")]]
 #define ISARRAY(len) [[clang::pulse("array:" STR(len)"|END")]]
@@ -12,3 +14,18 @@
 #define CONCAT_IND(x, y) x ## y
 #define CONCAT(x, y) CONCAT_IND(x, y)
 #define INCLUDE(...) [[clang::pulse("includes:" STR(__VA_ARGS__) "|END")]] void CONCAT(__pulse_include_anchor_, __COUNTER__) (void) {}
+#else 
+#define STR(...)
+#define REQUIRES(s)
+#define ENSURES(s)
+#define RETURNS(s)
+#define ISARRAY(len)
+#define INVARIANTS(...)
+#define LEMMA(l)
+#define ERASED_ARG(arg)
+#define ASSERT(message)
+#define HEAPALLOCATED(message)
+#define CONCAT_IND(x, y)
+#define CONCAT(x, y)
+#define INCLUDE(...)
+#endif
