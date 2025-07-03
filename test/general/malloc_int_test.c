3104ed1e17f88502962cf3ee0b170911bccb2ba4
Angelica Moreira <48168649+angelica-moreira@users.noreply.github.com>
2025-07-03 13:24:42 -0700

    Adding missing tests (created by Vidush) that were living in lit-test folder

---
+++ test/general/malloc_int_test.c
@@ -0,0 +1,11 @@
+#include "../../include/PulseMacros.h"
+
+RETURNS(r:ref int32)
+ENSURES(r |-> v)
+ENSURES(freeable r)
+int* new_heap_ref(int v)
+{
+    int *r = (int*)malloc(sizeof(int));
+    *r = v;
+    return r;
+}
