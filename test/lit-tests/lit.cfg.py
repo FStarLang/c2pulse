7ebb1e82dd2d76a19d3e1258a160dc7336aa9059
Angelica Moreira <48168649+angelica-moreira@users.noreply.github.com>
2025-07-02 18:06:44 -0700

    renaming

--- test/lit-tests/lit.cfg.py
+++ test/lit-tests/lit.cfg.py
@@ -7,12 +7,12 @@ config.suffixes = [".c"]
 config.test_source_root = os.path.dirname(__file__)
 config.test_exec_root = config.test_source_root
 
-llvm_bin_path = os.path.join(config.test_source_root, '..', 'external', 'llvm-project', 'build', 'bin')
+llvm_bin_path = os.path.join(config.test_source_root, '../..', 'external', 'llvm-project', 'build', 'bin')
 
 # Get the paths from environment variables or provide default fallback
 c2pulse_path = os.environ.get("C2PULSE_PATH", os.path.join(llvm_bin_path, 'c2pulse'))
 filecheck_path = os.environ.get("FILECHECK", os.path.join(llvm_bin_path, "FileCheck"))
-fstar_run_script = os.environ.get("FSTAR_RUN_SCRIPT", os.path.join(config.test_source_root, '..', 'run_fstar.sh'))
+fstar_run_script = os.environ.get("FSTAR_RUN_SCRIPT", os.path.join(config.test_source_root, '../..', 'run_fstar.sh'))
 
 # Substitutions
 config.substitutions.append(("%c2pulse", c2pulse_path))
