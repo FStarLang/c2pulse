e21ca1e6d895a79cea3e1521aba88677d7af7e18
Vidush Singhal <vidush.sl@gmail.com>
2025-07-07 11:37:02 -0700

    add expected failures in test script

--- test_runner.sh
+++ test_runner.sh
@@ -7,6 +7,20 @@ DEFAULT_PATHS=(
   "./test/issue-related/"
 )
 
+SHOULD_FAIL=(
+ "./test/issue-related/issue27_test.c"
+ "./test/issue-related/issue22_test.c" 
+)
+
+is_expected_to_fail() {
+  for file in "${SHOULD_FAIL[@]}"; do
+    if [[ "$file" == "$1" ]]; then
+      return 0
+    fi
+  done
+  return 1
+}
+
 # Check if any arguments are provided, if not use default paths.
 if [ "$#" -lt 1 ]; then
   echo "The user did not provide any paths, running tests on known directories!"
@@ -25,9 +39,17 @@ process_c_file() {
   if bash $HERE/scripts/run_test.sh "$cfile" &> /dev/null ; then
     echo "  ✔ Success: $cfile"
   else
-    echo "  ❌ Failed: $cfile"
-    FAILED_FILES+=("$cfile")
+    if is_expected_to_fail $cfile; then
+	    echo " ✔ Success expected failure: $cfile"
+    else
+	    echo "  ❌ Failed: $cfile"
+	    FAILED_FILES+=("$cfile")
+    fi
   fi
+}
+
+normalize_path() {
+  echo "${1%/}"
 }
 
 echo "▶ Generating Pulse code from C code and running F* on them"
@@ -39,7 +61,8 @@ for path in "$@"; do
   elif [ -d "$path" ]; then
     echo "Processing .c files in directory: $path"
     shopt -s nullglob
-    for cfile in "$path"/*.c; do
+    clean_path=$(normalize_path "$path")
+    for cfile in "$clean_path"/*.c; do
       process_c_file "$cfile"
     done
     shopt -u nullglob
