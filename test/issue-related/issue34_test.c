3ef54e36ebbb00edd9f70d151a8c2bca965aba97
Vidush Singhal <vidush.sl@gmail.com>
2025-07-09 13:21:15 -0700

    Test case for #34; Closes #34

---
+++ test/issue-related/issue34_test.c
@@ -0,0 +1,16 @@
+#include <stdint.h>
+#include "../include/PulseMacros.h"
+
+RETURNS(b:FStar.UInt32.t)
+uint32_t integer_promotion()
+{
+  uint32_t x = 1;
+  if (x == 1)
+  {
+    return 0;
+  }
+  else
+  {
+    return 0;
+  }
+}
