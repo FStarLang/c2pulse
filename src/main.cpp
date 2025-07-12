6311b08b9bcd24e12fe65950a4c606d038cdc106
Angelica Moreira <48168649+angelica-moreira@users.noreply.github.com>
2025-07-12 15:56:59 -0700

    adding the map of macro events and showing how to retrieve the info to pass to the other stages

--- src/main.cpp
+++ src/main.cpp
@@ -1,22 +1,22 @@
 #include "ExprLocationAnalyzer.h"
-// #include "clang/Frontend/FrontendActions.h"
-// #include "MacroFrontendAction.h"
 #include "MacroFrontendActionFactory.h"
 #include "PulseASTGenerator.h"
+#include "Globals.h"
+#include "FileIDHash.h"
+
 #include "clang/Tooling/CommonOptionsParser.h"
 #include "clang/Tooling/Tooling.h"
 #include "llvm/Support/Debug.h"
-#include <fstream>
+#include <functional>
+
 #include <unistd.h>
 #include <memory>
-
-#include "Globals.h"
-
-#define DEBUG_TYPE "expr-analyzer"
+// #include <vector>
+#include <unordered_map>
+#include <map>
 
 using namespace clang;
 using namespace clang::tooling;
-
 int GlobalGenSymCounter = 0;
 
 const char* pulseProofTermFromC = "__pulseproofterm";
@@ -37,14 +37,13 @@ llvm::cl::opt<std::string>
       llvm::cl::init("both"),
       llvm::cl::cat(ToolCategory));
 
-
 enum class TransformModeEnum {
     LocOnly,
     PulseOnly,
     Both,
 };
 
-TransformModeEnum parseTransformMode(const std::string &modeStr) {
+static TransformModeEnum parseTransformMode(const std::string &modeStr) {
     if (modeStr.compare("loc") == 0) return TransformModeEnum::LocOnly;
     if (modeStr.compare("pulse") == 0) return TransformModeEnum::PulseOnly;
     return TransformModeEnum::Both;
@@ -65,12 +64,17 @@ int main(int argc, const char **argv) {
         OptionsParser->getCompilations(), SourceFiles
     );
 
-    std::vector<MacroEventInfo> macroEventsVec;
-    // int Result = Tool->run(newFrontendActionFactory<SyntaxOnlyAction>().get());
-    // int Result =  Tool->run(newFrontendActionFactory<MacroFrontendAction>().get());
-    auto Factory = std::make_unique<MacroFrontendActionFactory>(macroEventsVec);
+    // std::vector<MacroEventInfo> macroEventsVec;
+    // auto Factory = std::make_unique<MacroFrontendActionFactory>(macroEventsVec);
+    
+    // I decided to use a map to store macro events by file ID and source location (range)
+    // This allows for better organization and retrieval of macro events across different 
+    // files and locations. And also allows for events with same range in different files
+    // be stored separately, I am trying to avoid collisions. I believe this is useful for 
+    // large projects with many files.
+    std::unordered_map<FileID, std::map<unsigned, MacroEventInfo>> macroInfoMap;
+    auto Factory = std::make_unique<MacroFrontendActionFactory>(macroInfoMap);
     int Result = Tool->run(Factory.get());
-
     if (Result != 0) {
         llvm::errs() << "c2pulse cannot compile the C files due to a syntax error!\n";
         llvm::errs() << "Exiting..." << "\n";
@@ -78,36 +82,29 @@ int main(int argc, const char **argv) {
     }
     llvm::outs() << "Success: Syntax validated.\n";
 
-    auto *Action = Factory->getAction();
-    if (Action) {
-        int macroEventCount = macroEventsVec.size();
-        if (macroEventCount > 0) {
-            llvm::outs() << "Printing Macro Events from Main:\n";
-            llvm::outs() << "Number of macro events collected: "
-                        << macroEventCount << "\n";
+    // LLVM_DEBUG({
+    // int macroEventCount = macroEventsVec.size();
+    // if (macroEventCount > 0) {
+    //     llvm::outs() << "Printing Macro Events from Main:\n";
+    //     llvm::outs() << "Number of macro events collected: "
+    //                 << macroEventCount << "\n";
 
-            for (const auto &event : macroEventsVec) {
-                llvm::outs() << "Kind: " << toString(event.Kind) << "\n";
-                llvm::outs() << "Macro: " << event.MacroName << "\n";
-                llvm::outs() << "Expansion: " << event.ExpansionText << "\n";
-                llvm::outs() << "Location: Line: " << event.Line << ", Column: " << event.Column << "\n";
-                llvm::outs() << "Filename: " << event.FileName << "\n";
-                llvm::outs() << "Tokens:\n";
-                for (const auto &token : event.Tokens) {
-                    llvm::outs() << "  " << (token.IsParam ? "[param] " : "[macro] ")
-                                 << token.TokenText << " at Line "
-                                 << token.Line << ", Column "
-                                 << token.Column << "\n";
-                }
-                llvm::outs() << "----------------------\n";
-            }
-            
-        } else {
-            llvm::errs() << "No tracker available from MacroFrontendAction.\n";
-        }
-    } else {
-        llvm::errs() << "MacroFrontendAction not available.\n";
-    }
+    //     for (const auto &event : macroEventsVec) {
+    //         llvm::outs() << "Kind: " << toString(event.Kind) << "\n";
+    //         llvm::outs() << "Macro: " << event.MacroName << "\n";
+    //         llvm::outs() << "Expansion: " << event.ExpansionText << "\n";
+    //         llvm::outs() << "Location: Line: " << event.Line << ", Column: " << event.Column << "\n";
+    //         llvm::outs() << "Filename: " << event.FileName << "\n";
+    //         llvm::outs() << "Tokens:\n";
+    //         for (const auto &token : event.Tokens) {
+    //             llvm::outs() << "  " << (token.IsParam ? "[param] " : "[macro] ")
+    //                             << token.TokenText << " at Line "
+    //                             << token.Line << ", Column "
+    //                             << token.Column << "\n";
+    //         }
+    //         llvm::outs() << "----------------------\n";
+    //     } 
+    // }});
 
     LLVM_DEBUG({
         for (const auto &file : SourceFiles) {
@@ -119,9 +116,8 @@ int main(int argc, const char **argv) {
     // Syntax and semantic checks are performed internally by Clang during AST construction
     Tool->buildASTs(ASTList); 
 
-    DEBUG_WITH_TYPE(DEBUG_TYPE, llvm::dbgs()
-                                        << "Number of parsed ASTs: " 
-                                        << ASTList.size() << "\n"); 
+    LLVM_DEBUG( llvm::dbgs() << "Number of parsed ASTs: " 
+                             << ASTList.size() << "\n"); 
 
 
     TransformModeEnum mode = parseTransformMode(TransformMode);
@@ -138,14 +134,44 @@ int main(int argc, const char **argv) {
         }
 
         clang::ASTContext &Ctx = AST->getASTContext();
-        DEBUG_WITH_TYPE(DEBUG_TYPE, llvm::dbgs()
-                                        << "Processing AST for file: " 
-                                        << fileName << "\n");
+        LLVM_DEBUG( llvm::dbgs() << "Processing AST for file: " 
+                                 << fileName << "\n");
+        
+        clang::SourceManager &SM = Ctx.getSourceManager();
+        clang::FileID fileID = SM.getMainFileID();
+        // this is the map of events that you can pass to the PulseTransformer or ExprLocationAnalyzer
+        // it depends where you will decide to collect the source information
+        // I was wondering if we could have it modular, so we can have everything
+        // organized in such a way that it is easy to maintain and extend.
+        const std::map<unsigned, MacroEventInfo> &events = macroInfoMap[fileID]; 
+        LLVM_DEBUG( 
+        if (!events.empty()) {
+        llvm::outs() << "  Number of macro events collected: " << events.size() << "\n";
+        llvm::outs() << "----------------------\n";
+        for (const auto &e : events) {
+            const MacroEventInfo &macroInfo = e.second;
+            llvm::outs() << "Kind: " << toString(macroInfo.Kind) << "\n";
+            llvm::outs() << "Macro: " << macroInfo.MacroName << "\n";
+            llvm::outs() << "Expansion: " << macroInfo.ExpansionText << "\n";
+            llvm::outs() << "Location: Line: " << macroInfo.Line << ", Column: " << macroInfo.Column << "\n";
+            llvm::outs() << "Filename: " << macroInfo.FileName << "\n";
+            llvm::outs() << "Tokens:\n";
+            for (const auto &t : macroInfo.Tokens) {
+                llvm::outs() << "  " << (t.IsParam ? "[param] " : "[macro] ")
+                             << t.TokenText << " at Line " << t.Line
+                             << ", Column " << t.Column << "\n";
+            }
+            llvm::outs() << "----------------------\n";
+        }
+        llvm::outs() << "\n";
+        } else {
+            llvm::outs() << "  [No macro events found]\n";
+        }
+        );
 
         if (mode == TransformModeEnum::LocOnly || mode == TransformModeEnum::Both) {
             ExprLocationAnalyzer Analyzer(Ctx);
             Analyzer.analyze(Ctx.getTranslationUnitDecl());
-            Analyzer.printNodeInfoMap();
         }
 
         if (mode == TransformModeEnum::PulseOnly || mode == TransformModeEnum::Both) {
