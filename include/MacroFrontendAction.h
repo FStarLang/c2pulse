10c939a4bf8c2d73cceed961f9da661ef7049cc2
Angelica Moreira <48168649+angelica-moreira@users.noreply.github.com>
2025-07-12 15:48:18 -0700

    reordering includes and adding a map for macros per fileid and location

--- include/MacroFrontendAction.h
+++ include/MacroFrontendAction.h
@@ -1,14 +1,22 @@
 #pragma once
 
-#include "clang/Frontend/FrontendActions.h"
 #include "MacroCommentTracker.h"
 #include "MacroMetadata.h"
+#include "FileIDHash.h"
+
+#include "clang/Frontend/FrontendActions.h"
+
 #include <memory>
+// #include <vector>
+#include <unordered_map>
+#include <map>
 
 class MacroFrontendAction : public clang::SyntaxOnlyAction {
 public:
-    explicit MacroFrontendAction(std::vector<MacroEventInfo> &macroEvents)
-            : macroEventsVec(macroEvents) {}
+    // explicit MacroFrontendAction(std::vector<MacroEventInfo> &macroEvents)
+    //         : macroEventsVec(macroEvents) {}
+    explicit MacroFrontendAction(std::unordered_map<clang::FileID, std::map<unsigned, MacroEventInfo>> &macroEvents)
+        : macroInfoMap(macroEvents) {}
     std::unique_ptr<clang::ASTConsumer> CreateASTConsumer(clang::CompilerInstance &CI, llvm::StringRef InFile) override;
     MacroCommentTracker *getTracker() const { return TrackerRaw; }
 
@@ -17,7 +25,8 @@ protected:
     void EndSourceFileAction() override;
 
 private:
-    std::vector<MacroEventInfo> &macroEventsVec;
+    // std::vector<MacroEventInfo> &macroEventsVec;
+    std::unordered_map<clang::FileID, std::map<unsigned, MacroEventInfo>> &macroInfoMap;
     MacroCommentTracker * TrackerRaw = nullptr; // externally managed in main.cpp
 
 };
