ade12f6ec9155116b669724250712cc5f9f2c2b9
Vidush Singhal <vidush.sl@gmail.com>
2025-07-15 15:30:04 -0700

    add test for #55

---
+++ test/issue-related/issue55_test.c
@@ -0,0 +1,14 @@
+#include "../include/PulseMacros.h"
+#include <stddef.h>
+#include <stdbool.h>
+
+bool compare(ISARRAY() int *a1, ISARRAY() int *a2, size_t l)
+{
+    size_t i = 0;
+    while( (i < l) ? a1[i] == a2[i] : false )
+    INVARIANTS( invariant c. emp)
+    {
+        i = i + 1;
+    }
+    return (i == l);
+}
