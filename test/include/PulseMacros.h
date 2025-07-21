26da4c722847993ee99388675166d0ccd92e8715
Vidush Singhal <vidush.sl@gmail.com>
2025-07-21 08:44:27 -0700

    change lemma macro

--- test/include/PulseMacros.h
+++ test/include/PulseMacros.h
@@ -5,7 +5,7 @@
 #define RETURNS(s) [[clang::pulse("returns:" STR(s)"|END")]]
 #define ISARRAY(len) [[clang::pulse("array:" STR(len)"|END")]]
 #define INVARIANTS(...) [[clang::pulse("invariants:" STR(__VA_ARGS__)"|END")]]
-#define LEMMA(l) [[clang::pulse("lemma:" STR(l)"|END")]]
+#define LEMMA(l) [[clang::pulse("lemma:" STR(l)"|END")]]0
 #define ERASED_ARG(arg) [[clang::pulse("erased_arg:" STR(arg)"|END")]]
 #define ASSERT(message) [[clang::pulse("assert:" STR(message) "|END")]]
 #define HEAPALLOCATED(message) [[clang::pulse("heap_allocated:" STR(message) "|END")]]
