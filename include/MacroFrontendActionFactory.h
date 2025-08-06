8782573b7b2c438c728e716279ae554bc0422a64
Angelica Moreira <48168649+angelica-moreira@users.noreply.github.com>
2025-08-06 15:56:10 -0700

    stale comments

--- include/MacroFrontendActionFactory.h
+++ include/MacroFrontendActionFactory.h
@@ -12,15 +12,12 @@
 
 class MacroFrontendActionFactory : public clang::tooling::FrontendActionFactory {
 public:
-    // MacroFrontendActionFactory(std::vector<MacroEventInfo> &events)
-    //         : macroEventsVec(events) {}
     MacroFrontendActionFactory(std::unordered_map<clang::FileID, std::map<unsigned, MacroEventInfo>> &events)
         : macroInfoMap(events) {}
 
     MacroFrontendAction *getAction() const { return ActionPtr; }
 
     std::unique_ptr<clang::FrontendAction> create() override {
-        // auto Action = std::make_unique<MacroFrontendAction>(macroEventsVec);
         auto Action = std::make_unique<MacroFrontendAction>(macroInfoMap);
         ActionPtr = Action.get(); // Keep raw pointer for later access in main.cpp [I need to revisit this decision]
         return Action;
@@ -28,6 +25,5 @@ public:
 
 private:
     mutable MacroFrontendAction *ActionPtr = nullptr; 
-    // std::vector<MacroEventInfo> &macroEventsVec;
     std::unordered_map<clang::FileID, std::map<unsigned, MacroEventInfo>> &macroInfoMap;
 };
