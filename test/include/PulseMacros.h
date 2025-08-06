b7ce80116b5fcafc5630e3d9867b19246ac78bcb
Guido Martínez <mtzguido@gmail.com>
2025-08-05 23:15:02 -0700

    PulseMacros.h: also add an expression to ASSERT, similarly to the fix for #52

--- test/include/PulseMacros.h
+++ test/include/PulseMacros.h
@@ -8,7 +8,7 @@
 #define INVARIANTS(...) [[clang::pulse("invariants:" STR(__VA_ARGS__)"|END")]]
 #define LEMMA(l) [[clang::pulse("lemma:" STR(l)"|END")]]0
 #define ERASED_ARG(arg) [[clang::pulse("erased_arg:" STR(arg)"|END")]]
-#define ASSERT(message) [[clang::pulse("assert:" STR(message) "|END")]]
+#define ASSERT(message) [[clang::pulse("assert:" STR(message) "|END")]]0
 #define HEAPALLOCATED(message) [[clang::pulse("heap_allocated:" STR(message) "|END")]]
 #define EXPECT_FAILURE(...)                                                    \
   [[clang::pulse("expect_failure:" STR(__VA_ARGS__) "|END")]]
