ca8627198a35e1f0d96970b76606a1366568e9aa
Gabriel Ebner <gebner@gebner.org>
2025-08-08 22:18:03 -0700

    Add missing c2pulse.config.json

--- test/general/bool_add.c
+++ test/general/bool_add.c
@@ -1,7 +1,8 @@
 #include <stdbool.h>
 #include "../include/PulseMacros.h"
 
-RETURNS(Int32.t)
+RETURNS(r: Int32.t)
+ENSURES(pure (v r == (if a then 1 else 0) + (if b then 1 else 0)))
 int foo(bool a, bool b){
 	return a + b;
 }
