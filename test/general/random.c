78688e81f015ca2481c7b5a7b8b5c91b89d69799
Vidush Singhal <vidush.sl@gmail.com>
2025-08-11 15:16:50 -0700

    fix test

--- test/general/random.c
+++ test/general/random.c
@@ -12,7 +12,7 @@ typedef struct _s1 {
     bool arr[]; 
 } s1;
 
-//PRESERVES(exists* s1_s. s1_pred x1 s1_s)
+PRESERVES(exists* s1_s. s1_pred x1 s1_s)
 void foo(s1 *x1){
     LEMMA(s1_explode (!x1));
     LEMMA(pts_to_len ((!(!x1)).bytes));
