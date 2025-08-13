0f71ce8c2925a0d8a32522e4cd5a0bdcd32f4c87
Angelica Moreira <48168649+angelica-moreira@users.noreply.github.com>
2025-08-12 21:16:58 -0700

    adding annotations to a deref example

--- test/memory_bugs/toy_examples/pointer.c
+++ test/memory_bugs/toy_examples/pointer.c
@@ -1,4 +1,11 @@
-void null_deref() {
-    int *p = NULL;
-    *p = 1;  // UB: Dereferencing NULL.
+#include <stddef.h>
+#include "../../include/PulseMacros.h"
+
+ERASED_ARG(#v: Pulse.Lib.C.Int32.int32)   
+REQUIRES(p |-> v)
+ENSURES(exists* (w: Pulse.Lib.C.Int32.int32).
+        (p |-> w) ** pure Pulse.Lib.C.Int32.(as_int w == 1))
+void deref(int *p) {
+    *p = 1;
+    p = NULL;
 }
