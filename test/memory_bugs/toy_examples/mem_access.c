08274504e36beaf8f7bd595d7c88888dbfa71e49
Angelica Moreira <48168649+angelica-moreira@users.noreply.github.com>
2025-08-12 21:16:27 -0700

    adding annotations

--- test/memory_bugs/toy_examples/mem_access.c
+++ test/memory_bugs/toy_examples/mem_access.c
@@ -1,6 +1,10 @@
-#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include "../../include/PulseMacros.h"
+
 
 void buffer_overflow() {
-    int arr[5];
+    ISARRAY(5) int *arr = (int*) malloc(5 * sizeof(int));
     arr[10] = 42;  // UB: Out-of-bounds access.
+    free(arr);
 }
