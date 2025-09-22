bbd73cd27bd99e02edb96641131c686bb7f8ebf6
Gabriel Ebner <gebner@gebner.org>
2025-09-22 16:11:46 -0700

    Fix macros.

--- examples/c2pulse.h
+++ examples/c2pulse.h
@@ -5,14 +5,15 @@
 #define _include_pulse(...) [[clang::annotate("c2pulse-includes", __COUNTER__)]] \
     void __c2pulse_concat(__c2pulse_include_anchor_, __COUNTER__) (void) {}
 
-#define _requires(p) __attribute__((annotate("c2pulse-requires", __COUNTER__)))
-#define _ensures(p) __attribute__((annotate("c2pulse-ensures", __COUNTER__)))
+#define __capture_args(args) __COUNTER__
+
+#define _requires(p) __attribute__((annotate("c2pulse-requires", __capture_args(p))))
+#define _ensures(p) __attribute__((annotate("c2pulse-ensures", __capture_args(p))))
 
 #define _plain [[clang::annotate("c2pulse-plain")]]
 #define _consumes [[clang::annotate("c2pulse-consumes")]]
 
-#define __capture_args(args...) __COUNTER__
-#define _inline_pulse(args...) _inline_pulse(_capture_args(args...))
+#define _inline_pulse(args) _inline_pulse(__capture_args(args))
 
 #else
 
