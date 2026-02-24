d6ef950da13a18dab24fffe63077460b1785f6bc
Gabriel Ebner <gebner@gebner.org>
2026-02-24 21:01:57 +0000

    Improve test.

--- test/goto_fail.c
+++ test/goto_fail.c
@@ -8,3 +8,15 @@ void cleanup_on_error(int *p)
     *p = 42;
 fail:;
 }
+
+int cleanup_on_error2()
+    _ensures((_specint) return == 30)
+{
+    int p = 0;
+    if (p != 0)
+        goto fail;
+    p = 42;
+fail: _ensures(_live(p))
+    p = 30;
+    return p;
+}
