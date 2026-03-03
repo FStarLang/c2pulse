c32d5d37de3d627d4ad114faf336ec09c29ee5e5
Gabriel Ebner <gebner@gebner.org>
2026-03-03 00:27:24 +0000

    Add test.

--- test/array.c
+++ test/array.c
@@ -7,3 +7,11 @@ void foo(unsigned a[])
   a[0] = 42;
   a[1] = a[0] + 67;
 }
+
+void ptr_attr(_array unsigned *a)
+  _requires(a._length == 2)
+  _ensures(a._length == 2 && a[0] == 42)
+{
+  a[0] = 42;
+  a[1] = a[0] + 67;
+}
