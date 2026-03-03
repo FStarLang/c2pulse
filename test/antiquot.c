227fa99393b138c2f5ea89eb9301097785215390
Gabriel Ebner <gebner@gebner.org>
2026-03-03 18:29:06 +0000

    Improve test.

--- test/antiquot.c
+++ test/antiquot.c
@@ -1,7 +1,8 @@
 #include "c2pulse.h"
 
 void test_rvalue_antiquot(int *x)
-    _ensures((_slprop) _inline_pulse(pure ($(*x) = $(*x))))
+    _ensures((_slprop) _inline_pulse(pure (Int32.v $(*x) > 0)))
 {
-    return;
+    _assert((_slprop) _inline_pulse($&(x) |-> $(x)));
+    *x = 6 + 7;
 }
