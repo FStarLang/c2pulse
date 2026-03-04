21580dc67f5623a0911cb3c7819df6dab30ec9e0
Gabriel Ebner <gebner@gebner.org>
2026-03-04 21:09:23 +0000

    Add test for issue 71.
    Fixes #71.

---
+++ test/issue71.c
@@ -0,0 +1,21 @@
+#include "c2pulse.h"
+#include <stdint.h>
+
+void incr1(int *x)
+    _requires(*x + 1 < INT32_MAX)
+{
+    *x = *x + 1;
+}
+
+void incr2(int *x)
+    _requires(*x + (_specint) 1 < INT32_MAX)
+    _requires(*x + (int) 1 < INT32_MAX)
+{
+    *x = *x + 1;
+}
+
+void incr3(int *x)
+    _requires(*x + (_specint) 1 < INT32_MAX)
+{
+    *x = *x + 1;
+}
