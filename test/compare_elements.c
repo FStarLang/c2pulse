dc80ebf541a96024fcb280a9c1a01afb8b0a254a
Gabriel Ebner <gebner@gebner.org>
2026-03-04 10:30:01 -0800

    Improve tests.

---
+++ test/compare_elements.c
@@ -0,0 +1,22 @@
+#include "c2pulse.h"
+#include <stdbool.h>
+#include <stddef.h>
+
+bool compare_elems(_array int *a, _array int *b, size_t len)
+  _requires(a._length == len && b._length == len)
+  _preserves_value(a._length)
+  _preserves_value(b._length)
+  // TODO: return == _forall doesn't work because of bool/prop mismatch
+  _ensures(return ==> _forall(size_t i, i < len ==> a[i] == b[i]))
+  _ensures(_forall(size_t i, i < len ==> a[i] == b[i]) ==> return)
+{
+  size_t i = 0;
+  while (i < len)
+    _invariant(_live(i))
+    _invariant(i <= len)
+    _invariant(_forall(size_t j, j < i ==> a[j] == b[j]))
+  {
+    if (a[i] != b[i]) return false;
+  }
+  return true;
+}
--- test/reverse_test.c
+++ test/reverse_test.c
@@ -4,6 +4,7 @@
 
 void reverse(_array uint32_t *arr, size_t len)
   _preserves(arr._length == len)
+  _ensures(_forall(size_t j, j < len ==> arr[j] == _old(arr[len - j - 1])))
 {
   size_t i = 0;
   while (i < len / 2)
@@ -11,6 +12,8 @@ void reverse(_array uint32_t *arr, size_t len)
     _invariant(_live(*arr))
     _invariant(arr._length == len)
     _invariant(i <= len / 2)
+    _invariant(_forall(size_t j, j < i || (len - 1 - i < j && j < len) ==> arr[j] == _old(arr[len - j - 1])))
+    _invariant(_forall(size_t j, i <= j && j < len - i ==> arr[j] == _old(arr[j])))
   {
     size_t j = len - 1 - i;  
     uint32_t tmp = arr[i];
