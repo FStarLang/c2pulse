81c18448b73f030d165cad8f536157746d11c781
Gabriel Ebner <gebner@gebner.org>
2026-03-07 05:02:44 +0000

    Improve test.

--- test/default_test.c
+++ test/default_test.c
@@ -10,6 +10,11 @@ _pure int zero;
 
 _pure point zero_point;
 
+void test_globals() {
+    _assert(zero == 0);
+    _assert(zero_point.x == 0);
+}
+
 void test_calloc_ref(void) {
     int *p = (int *) calloc(1, sizeof(int));
     _assert(*p == 0);
