9654796a4b9e1532923994dd34ca6b24452f8107
Gabriel Ebner <gebner@gebner.org>
2026-03-10 23:57:23 +0000

    Improve test.

--- test/out_param.c
+++ test/out_param.c
@@ -1,6 +1,15 @@
 #include "c2pulse.h"
 
 void init_value(_out int *x)
+    _ensures(*x == 42)
 {
     *x = 42;
+}
+
+int call_init_value()
+    _ensures(return == 42)
+{
+    int x;
+    init_value(&x);
+    return x;
 }
