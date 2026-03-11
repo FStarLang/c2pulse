10a97f02b90fcd7c7f9a3c605778d3cc0580fed9
Gabriel Ebner <gebner@gebner.org>
2026-03-11 18:45:15 +0000

    Clean up tests.

--- test/logical_int_ops.c
+++ test/logical_int_ops.c
@@ -1,13 +1,3 @@
-// Tests-todo: Logical operators on integer types (non-bool)
-//
-// Missing features: &&, ||, ! on int operands (C coerces 0 → false, nonzero → true)
-// Also: double-negation !!x as integer-to-bool idiom
-// Prototype tests: test_logicals_int.c, issue54_test_3.c, issue_anon_1.c
-//
-// In C, && and || are defined to work on any scalar type, converting
-// to bool implicitly. c2pulse currently only supports these on bool/slprop.
-// Proposed: insert implicit int-to-bool coercion (x != 0) before logical ops.
-
 #include "c2pulse.h"
 #include <stdint.h>
 
