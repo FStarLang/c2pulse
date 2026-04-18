3c0e46ebbfc48ba33748ee846a4de2d49db91396
Gabriel Ebner <gebner@gebner.org>
2026-04-18 01:17:14 +0000

    Improve test.

--- test/ptr_eq.c
+++ test/ptr_eq.c
@@ -4,10 +4,14 @@
 #include <stdbool.h>
 #include <stdint.h>
 
-bool ptrs_equal(_plain int32_t *a, _plain int32_t *b) {
+bool ptrs_equal(_plain int32_t *a, _plain int32_t *b)
+    _ensures(return == (a == b))
+{
     return (a == b);
 }
 
-bool ptrs_not_equal(_plain int32_t *a, _plain int32_t *b) {
+bool ptrs_not_equal(_plain int32_t *a, _plain int32_t *b)
+    _ensures(return != (a == b))
+{
     return (a != b);
 }
