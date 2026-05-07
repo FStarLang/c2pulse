46446ced330282b075bb4960ce3e5805362cc310
Gabriel Ebner <gebner@gebner.org>
2026-05-07 01:33:55 +0000

    Improve test.

--- test/opaque_type.c
+++ test/opaque_type.c
@@ -9,6 +9,9 @@ _include_pulse(
   let empty_list : $type(my_list) = ([] #Int32.t)
 )
 
+// Basic _let usage
+_let(my_list empty_list(), (my_list) _inline_pulse([]))
+
 // Use it as a _let return type with _inline_pulse in the body
 _let(my_list make_singleton(int x),
   (my_list) _inline_pulse(
