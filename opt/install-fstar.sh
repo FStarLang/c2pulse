3f6feb5827a35ca66717b5a19e2aaae679582792
Gabriel Ebner <gebner@gebner.org>
2026-05-19 17:04:44 +0000

    Update Pulse.

--- opt/install-fstar.sh
+++ opt/install-fstar.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-curl -fsSL https://aka.ms/install-fstar | bash -s -- --nightly --version 2026-05-13 "$@"
+curl -fsSL https://aka.ms/install-fstar | bash -s -- --nightly --version 2026-05-19 "$@"
